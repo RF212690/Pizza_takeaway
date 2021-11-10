85d3c6afbabb381445f89f286d9890b14f700548
RF212690 <90518866+RF212690@users.noreply.github.com>
2021-11-10 08:56:40 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -30,71 +30,72 @@ public class Main {
         valid=false;
         String temp="";
         String[] toppings= {"pepperoni","cheese","mushrooms","onions","bacon","green peppers","spinach","black olives",
-        "sausage","pineapple"};
+                "sausage","pineapple"};
         int[] toppingsnum= new int[10];
         ArrayList<String> usertoppings= new ArrayList<>();
         boolean found=false;
-            do{
-                try{
-                    System.out.println("type help to get a list of toppings, type end if you're satisfied.\n"
-                    +"Insert topping: ");
-                    temp=input.next();
-                }catch(Exception e){
-                    System.out.println(e);
-                    input.next();
+        do{
+            try{
+                System.out.println("type help to get a list of toppings, type end if you're satisfied.\n"
+                        +"Insert topping: ");
+                temp=input.next();
+            }catch(Exception e){
+                System.out.println(e);
+                input.next();
+            }
+            if(temp.equalsIgnoreCase("end")){
+                valid=true;
+            }
+            else if(temp.equalsIgnoreCase("help")){
+                System.out.println("List of our toppings: ");
+                for (int i = 0; i < toppings.length; i++) {
+                    System.out.print(toppings[i]+", ");
                 }
-                if(temp.equalsIgnoreCase("end")){
-                    valid=true;
-                }
-                else if(temp.equalsIgnoreCase("help")){
-                    System.out.println("List of our toppings: ");
-                    for (int i = 0; i < toppings.length; i++) {
-                        System.out.print(toppings[i]+", ");
-                    }
-                    System.out.println();
-                }
-                else{
-                    for (int i = 0; i < toppings.length; i++) {
-                        if(toppings[i].equalsIgnoreCase(temp)){
-                            found=true;
-                            if(toppingsnum[i]<2){
-                                usertoppings.add(temp.toLowerCase(Locale.ROOT));
-                                toppingsnum[i]++;
-                                System.out.println("topping added to your pizza");
-                            }
-                            else{
-                                System.out.println("You selected this topping too many times");
-                            }
+                System.out.println();
+            }
+            else{
+                for (int i = 0; i < toppings.length; i++) {
+                    if(toppings[i].equalsIgnoreCase(temp)){
+                        found=true;
+                        if(toppingsnum[i]<2){
+                            usertoppings.add(temp.toLowerCase(Locale.ROOT));
+                            toppingsnum[i]++;
+                            System.out.println("topping added to your pizza");
+                        }
+                        else{
+                            System.out.println("You selected this topping too many times");
                         }
                     }
-                    if(!found){
-                        System.out.println("Not one of our toppings");
-                    }
-                    else{
-                        found=false;
-                    }
                 }
+                if(!found){
+                    System.out.println("Not one of our toppings");
+                }
+                else{
+                    found=false;
+                }
+            }
 
-            }while(!valid && usertoppings.size()<20);
-            usertoppings.stream().sorted();
-            if(usertoppings.size()==20){
+        }while(!valid && usertoppings.size()<20);
+        usertoppings.stream().sorted();
+        if(usertoppings.size()==20){
             System.out.println("You inserted the maximum amount of toppings");
-            }
-            if(usertoppings.size()>=2){
-                total+=(usertoppings.size()-2)*1.25;
-            }
+        }
+        if(usertoppings.size()>=2){
+            total+=(usertoppings.size()-2)*1.25;
+        }
         for (int i = 0; i < usertoppings.size(); i++) {
             for (int j = i+1; j < usertoppings.size(); j++) {
                 if(usertoppings.get(i).equals(usertoppings.get(j))){
                     usertoppings.remove(j);
+                    usertoppings.set(i,usertoppings.get(i)+"x2");
                 }
             }
         }
-            System.out.println("List of your toppings: ");
-            for (int i = 0; i < usertoppings.size(); i++) {
-                System.out.print(usertoppings.get(i)+"\n");
-            }
-            System.out.println();
+        System.out.println("List of your toppings: ");
+        for (int i = 0; i < usertoppings.size(); i++) {
+            System.out.print(usertoppings.get(i)+"\n");
+        }
+        System.out.println();
         System.out.println("the total cost is: "+total);
     }
 }
